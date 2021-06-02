37efe3b3103829f1288f9549e2834322ae87991c
epsilam <2.7182@protonmail.com>
2021-06-03 01:54:07 +0200

    Updated

--- src/python/computegamma.py
+++ src/python/computegamma.py
@@ -19,11 +19,6 @@ class ProteinProjection:
                                             # carbohydrates) and leaves only
                                             # the polymers
 
-        self.backboneCrossings = {}      # a dictionary containing crossings of
-                                         # bonds on the backbone. the element
-                                         # (i,j) is included if the bond
-                                         # between i and i+1 crosses the bond
-                                         # between j and j+1.
 
         self.resNums = np.unique(self.backbone.getResnums())
 
@@ -54,10 +49,31 @@ class ProteinProjection:
                           # in a given projection using some
                           # simple linear algebra.
 
-        self.proteinStructureBonds = set() # a set of pairs of residues which
-                                         # are bonded via a non-peptide bond.
 
-    def _checkBondCrossing(self, resi1, resi2, resj1, resj2):
+        self.backboneCrossings = {}      # a dictionary containing crossings of
+                                         # bonds on the backbone. the key
+                                         # (i,i+1,j,j+1) is included if the bond
+                                         # between i and i+1 crosses the bond
+                                         # between j and j+1.
+
+
+        self.proteinStructureBonds = set() # a set of pairs of residues which
+                                           # are bonded via a non-peptide bond.
+
+                                         # crossings between a bond on the
+                                         # backbone (peptide) and a secondary/
+                                         # tertiary structure bond.
+                                         # the element (i1,i2,j1,j2) is included
+                                         # if the bond between i1 and i2 crosses
+                                         # the bond between j1 and j2
+        self.structureWithBackboneCrossings = {}
+
+                                         # crossings between two secondary/
+                                         # tertiary structure bonds. same format
+                                         # as above.
+        self.structureCrossings = {}
+
+    def _checkBondCrossing(self, resi1, resi2, resj1, resj2, crossingDict):
         A = self.projectedResCoords[resi1]
         B = self.projectedResCoords[resi2]
         C = self.projectedResCoords[resj1]
@@ -70,7 +86,7 @@ class ProteinProjection:
         u = np.cross(E,R) / crossRS
         if crossRS != 0:
             if 0 <= t <= 1 and 0 <= u <= 1:
-                self.backboneCrossings[(resi1,resj1)] = A + t * R
+                crossingDict[(resi1,resi2,resj1,resj2)] = A + t * R
 
                           # for each pair of two successive residues (i.e., two
                           # residues with a peptide bond), check if there is a
@@ -82,19 +98,52 @@ class ProteinProjection:
             for jdx in range(idx+2,len(self.resNums)-1):
                 resj1 = self.resNums[jdx]
                 resj2 = self.resNums[jdx+1]
-                self._checkBondCrossing(resi1,resi2,resj1,resj2)
+                self._checkBondCrossing(resi1,resi2,resj1,resj2,
+                                        self.backboneCrossings)
 
                           # find all bonds corresponding to the secondary and
                           # tertiary structure of the protein
                           # (excludes peptide bonds)
     def findProteinStructureBonds(self):
-        self.p.inferBonds()
+        self.p.inferBonds(max_bond=2*1.221)
         for bond in self.backbone.iterBonds():
             bondAtomPair = bond.getAtoms()
             r1 = bondAtomPair[0].getResnum()
             r2 = bondAtomPair[1].getResnum()
             if r1 != r2 and r1 != r2 + 1 and r1 != r2 - 1:
                 self.proteinStructureBonds.add((r1,r2))
+
+
+                            # find crossings between secondary/tertiary
+                            # structure bonds.
+    def findStructureCrossings(self):
+        bonds = list(self.proteinStructureBonds)
+        for i in range(len(bonds)):
+            for j in range(i+1,len(bonds)):
+                resi1 = bonds[i][0]
+                resi2 = bonds[i][1]
+                resj1 = bonds[j][0]
+                resj2 = bonds[j][1]
+                if resi1 in (resj1,resj2) or resi2 in (resj1,resj2):
+                    continue
+                self._checkBondCrossing(resi1,resi2,resj1,resj2,
+                                        self.structureCrossings)
+
+                            # find crossings between peptide bonds and
+                            # secondary/tertiary structure bonds.
+    def findStructureWithBackboneCrossings(self):
+        bonds = list(self.proteinStructureBonds)
+        for i in range(len(bonds)):
+            for j in range(len(self.resNums)-1):
+                resi1 = bonds[i][0]
+                resi2 = bonds[i][1]
+                resj1 = self.resNums[j]
+                resj2 = self.resNums[j+1]
+                if resi1 in (resj1,resj2) or resi2 in (resj1,resj2):
+                    continue
+                self._checkBondCrossing(resi1,resi2,resj1,resj2,
+                                        self.structureWithBackboneCrossings)
+
 
     def plotCrossings(self):
         fig, ax = pl.subplots()
@@ -105,7 +154,7 @@ class ProteinProjection:
         lines = []
         for i in range(len(rc)-1):
             lines.append((rc[i],rc[i+1]))
-        lc = mc.LineCollection(lines, linewidths=2, colors=[(0,1,0,1)])
+        lc = mc.LineCollection(lines, linewidths=2, colors=[(0.2,0.8,0,1)])
         ax.add_collection(lc)
 
         lines = []                  # plot secondary/tertiary structure bonds
@@ -113,14 +162,18 @@ class ProteinProjection:
             rc1 = tuple(self.projectedResCoords[bondedResPair[0]])
             rc2 = tuple(self.projectedResCoords[bondedResPair[1]])
             lines.append((rc1,rc2))
-        lc = mc.LineCollection(lines, linewidths=2, colors=[(0,0,1,1)])
+        lc = mc.LineCollection(lines, linewidths=2, colors=[(0,0,1,0.3)])
         ax.add_collection(lc)
 
+        crossingDict = {}
+        crossingDict.update(self.backboneCrossings)
+        crossingDict.update(self.structureCrossings)
+        crossingDict.update(self.structureWithBackboneCrossings)
         crossCoords = []                # plot crossings
-        for key,value in self.backboneCrossings.items():
+        for key,value in crossingDict.items():
             crossCoords.append(value)
         xCrossCoords, yCrossCoords = zip(*crossCoords)
-        ax.scatter(xCrossCoords,yCrossCoords, c=[(1,0,0,1)])
+        ax.scatter(xCrossCoords,yCrossCoords, c=[(1,0,0,1)], s=30, zorder=3)
 
         ax.autoscale()
         ax.margins(0.1)
