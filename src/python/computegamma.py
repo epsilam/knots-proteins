f7e01b3c5c714fca21bba2103708089b6915735a
epsilam <2.7182@protonmail.com>
2021-06-03 00:04:52 +0200

    Started hydrogen bond detection

--- src/python/computegamma.py
+++ src/python/computegamma.py
@@ -14,8 +14,10 @@ class ProteinProjection:
     def __init__(self, proteinName: str, angle1: float, angle2: float):
 
         self.p = pd.parsePDB(proteinName)
-        backbone = self.p.select('protein') # removes unnecessary H2O molecules,
-                                            # leaving only the backbone residues
+        self.backbone = self.p.select('protein') # removes irrelevant structures
+                                            # (such as water molecules and
+                                            # carbohydrates) and leaves only
+                                            # the polymers
 
         self.backboneCrossings = {}      # a dictionary containing crossings of
                                          # bonds on the backbone. the element
@@ -23,7 +25,7 @@ class ProteinProjection:
                                          # between i and i+1 crosses the bond
                                          # between j and j+1.
 
-        self.resNums = np.unique(backbone.getResnums())
+        self.resNums = np.unique(self.backbone.getResnums())
 
         self.resCoords = {} # dictionary of residue coordinates as averages
                             # of atom coordinates in the respective residue
@@ -33,7 +35,7 @@ class ProteinProjection:
                                      # of the residue projected onto the given
                                      # plane.
         for i in self.resNums:
-            res = backbone['A',i]
+            res = self.backbone['A',i]
             self.resCoords[i] = np.average(res.getCoords(),axis=0)
                                      # we rotate the residue coordinates by
                                      # the two angles and then project onto
@@ -51,7 +53,11 @@ class ProteinProjection:
                           # and resi2 crosses the bond between resj1 and resj2
                           # in a given projection using some
                           # simple linear algebra.
-    def checkBondCrossing(self, resi1, resi2, resj1, resj2):
+
+        self.proteinStructureBonds = set() # a set of pairs of residues which
+                                         # are bonded via a non-peptide bond.
+
+    def _checkBondCrossing(self, resi1, resi2, resj1, resj2):
         A = self.projectedResCoords[resi1]
         B = self.projectedResCoords[resi2]
         C = self.projectedResCoords[resj1]
@@ -69,34 +75,55 @@ class ProteinProjection:
                           # for each pair of two successive residues (i.e., two
                           # residues with a peptide bond), check if there is a
                           # crossing with another
-    def findBackboneCrossings(self):
+    def findBackbonePrimaryCrossings(self):
         for idx in range(len(self.resNums)-1):
             resi1 = self.resNums[idx]
             resi2 = self.resNums[idx+1]
             for jdx in range(idx+2,len(self.resNums)-1):
                 resj1 = self.resNums[jdx]
                 resj2 = self.resNums[jdx+1]
-                self.checkBondCrossing(resi1,resi2,resj1,resj2)
+                self._checkBondCrossing(resi1,resi2,resj1,resj2)
 
+                          # find all bonds corresponding to the secondary and
+                          # tertiary structure of the protein
+                          # (excludes peptide bonds)
+    def findProteinStructureBonds(self):
+        self.p.inferBonds()
+        for bond in self.backbone.iterBonds():
+            bondAtomPair = bond.getAtoms()
+            r1 = bondAtomPair[0].getResnum()
+            r2 = bondAtomPair[1].getResnum()
+            if r1 != r2 and r1 != r2 + 1 and r1 != r2 - 1:
+                self.proteinStructureBonds.add((r1,r2))
 
     def plotCrossings(self):
+        fig, ax = pl.subplots()
+
         rc = []                      # plot backbone
         for key,value in self.projectedResCoords.items():
             rc.append(tuple(value))
         lines = []
         for i in range(len(rc)-1):
-            lines.append([rc[i],rc[i+1]])
-        lc = mc.LineCollection(lines, linewidths=2)
-        fig, ax = pl.subplots()
+            lines.append((rc[i],rc[i+1]))
+        lc = mc.LineCollection(lines, linewidths=2, colors=[(0,1,0,1)])
         ax.add_collection(lc)
-        ax.autoscale()
-        ax.margins(0.1)
+
+        lines = []                  # plot secondary/tertiary structure bonds
+        for bondedResPair in self.proteinStructureBonds:
+            rc1 = tuple(self.projectedResCoords[bondedResPair[0]])
+            rc2 = tuple(self.projectedResCoords[bondedResPair[1]])
+            lines.append((rc1,rc2))
+        lc = mc.LineCollection(lines, linewidths=2, colors=[(0,0,1,1)])
+        ax.add_collection(lc)
 
         crossCoords = []                # plot crossings
         for key,value in self.backboneCrossings.items():
             crossCoords.append(value)
         xCrossCoords, yCrossCoords = zip(*crossCoords)
-        ax.scatter(xCrossCoords,yCrossCoords, c=(1,0,0,1))
+        ax.scatter(xCrossCoords,yCrossCoords, c=[(1,0,0,1)])
+
+        ax.autoscale()
+        ax.margins(0.1)
         fig.show()
 
 
--- src/python/main.py
+++ src/python/main.py
@@ -1,4 +1,7 @@
 from computegamma import *
 
-PP = ProteinProjection('1L2Y',0,np.pi/2)
-PP.findBackboneCrossings()
+#PP = ProteinProjection('1L2Y',0,np.pi/3)
+PP = ProteinProjection('7LT8',0,0)
+PP.findBackbonePrimaryCrossings()
+PP.findProteinStructureBonds()
+PP.plotCrossings()
