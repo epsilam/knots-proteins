37e268303d3f10ce33316176208c0557858dad18
epsilam <2.7182@protonmail.com>
2021-06-03 06:53:11 +0200

    changed example protein

--- src/python/main.py
+++ src/python/main.py
@@ -1,7 +1,7 @@
 from proteinprojection import *
 
-#PP = ProteinProjection('1L2Y',0,np.pi/3)
-PP = ProteinProjection('7LT8',0,0)
+PP = ProteinProjection('1L2Y',0,np.pi/4)
+#PP = ProteinProjection('7LT8',0,0)
 PP.findBackbonePrimaryCrossings()
 PP.findProteinStructureBonds()
 PP.findStructureCrossings()
