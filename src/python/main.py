e36ad04716725ce75ad4dcd032a3e7437af3be17
epsilam <2.7182@protonmail.com>
2021-07-19 04:07:27 +0200

    Removed old files

--- src/python/main.py
+++
@@ -1,9 +0,0 @@
-from proteinprojection import *
-
-PP = ProteinProjection('1L2Y',0,np.pi/3)
-#PP = ProteinProjection('7LT8',0,0)
-PP.findBackbonePrimaryCrossings()
-PP.findProteinStructureBonds()
-PP.findStructureCrossings()
-PP.findStructureWithBackboneCrossings()
-PP.plotCrossings()
