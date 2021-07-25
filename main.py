def6e753d322b7d4376a9159381594c008c836b2
epsilam <2.7182@protonmail.com>
2021-07-25 23:58:20 +0200

    Update

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.gamma import *
+
+P = ProteinGamma('1L2Y',0,np.pi/3)
+P.printGamma()
--- src/gamma.py
+++ src/gamma.py
@@ -1,13 +1,35 @@
 from src.proteingraph import *
+import sys
 
 class ProteinGamma(ProteinGraph):
     def __init__(self, proteinName: str, angle1: float, angle2: float):
         ProteinGraph.__init__(self, proteinName, angle1, angle2)
 
-        self.strandSegments = {}
-        self.merges = {}
+        self._labels = {} # dictionary containing labels for strands and
+                         # H-contacts. Converts from the vertex IDs of the
+                         # Graph class to labels used in Mathematica.
+        self._labelCounter = 0
 
-    def __nextVertex(self, prvStrand, prv, vert):
+    def __strandLabel(self, strand):
+        """Return ID of strand. If strand ID has not been encountered, a new ID
+        is created."""
+        if strand in self._labels:
+            return self._labels[strand]
+        ID = self._labelCounter
+        self._labelCounter = ID + 1
+        self._labels[strand] = ID
+        return ID
+
+    def __HcontactLabel(self, hvert):
+        if hvert in self._labels:
+            return self._labels[hvert]
+        ID = self._labelCounter
+        self._labelCounter = ID + 1
+        self._labels[hvert] = ID
+        return ID
+
+
+    def __nextVertex(self, prvStrandNum, prv, vert):
         """Returns a pair (nxt, s) where nxt is the ID of the next vertex from
         vert along the strand connected to prev, and s is the strand (1 or 2)
         along which we travel through vert. prvStrand is the strand (1 or 2)
@@ -25,11 +47,11 @@ class ProteinGamma(ProteinGraph):
             a vertex")
 
         # if the previous vertex along both strands is the same, we use the
-        # information passed by prvStrand.
+        # information passed by prvStrandNum.
         if prv1 == prv2:
             si0 = int(strandInfo[0])
             si2 = int(strandInfo[2])
-            if prvStrand == si0:
+            if prvStrandNum == si0:
                 if si2 == 1:
                     return (nxt1, 1)
                 elif si2 == 2:
@@ -48,30 +70,104 @@ class ProteinGamma(ProteinGraph):
             raise Exception("Error in obtaining next vertex")
 
 
-    def _vertexSequence(self):
+    def vertexSequence(self):
         """Returns a generator that yields all H- and X-vertices in the graph
         in order from the initial to the terminal vertex, together with the
-        strand of that vertex along which the generator travels. Return format
-        is (v,s) with v the vertex and s the strand."""
+        strand of that vertex along which the generator travels."""
 
-        ## check that the graph has been adequately pruned.
-        #if len(self.Sverts) != 1 or self.Sverts[0][1] is not None:
-        #    raise Exception("Error: unexpected vertices. Graph must be pruned.")
+        # Output format: [vType, vTypeAnnot, strand1, strand2, mergeStrand]
+        # where vType is 'H' or 'X',
+        # vTypeAnnot is either the label of the H-contact or '+' or '-'
+        # strand1,strand2 are the two strands of the vertex
+        # mergeStrand is the strand which we merge to the main strand
 
-        prvStrand = None
+        prvStrandNum = None
         prv = 0
         vert = self.Sverts[0][2]
-        (nxt, strand) = self.__nextVertex(prvStrand, prv, vert)
-        yield (vert, (None, nxt))
+        (nxt, strandNum) = self.__nextVertex(prvStrandNum, prv, vert)
+
+        if vert in self.Hverts:
+            (prv1,nxt1,prv2,nxt2) = self.Hverts[vert][1:5]
+            vType = ("H",self.__HcontactLabel(vert))
+        elif vert in self.Xverts:
+            (prv1,nxt1,prv2,nxt2) = self.Xverts[vert][1:5]
+            if self.Xverts[vert][5] == True:
+                vType = ("X","+")
+            else:
+                vType = ("X","-")
+
+        sID1 = self.__strandLabel((prv1,nxt1))
+        sID2 = self.__strandLabel((prv2,nxt2))
+        vertexReturn = (*vType, sID1, sID2)
+        ret = (*vType, sID1, sID2)
+        yield [*ret,ret[1+strandNum]]
 
         numVertices = len(self.Hverts) + len(self.Xverts)
         #for i in range(2 * numVertices - 1):
         while nxt is not None:
-            prvStrand = strand
+            prvStrandNum = strandNum
             prv = vert
             vert = nxt
-            (nxt, strand) = self.__nextVertex(prvStrand, prv, vert)
-            yield (vert, (prv, nxt))
+            (nxt, strandNum) = self.__nextVertex(prvStrandNum, prv, vert)
 
-    def _createMerge(self):
-        pass
+            if vert in self.Hverts:
+                (prv1,nxt1,prv2,nxt2) = self.Hverts[vert][1:5]
+                vType = ("H",self.__HcontactLabel(vert))
+            elif vert in self.Xverts:
+                (prv1,nxt1,prv2,nxt2) = self.Xverts[vert][1:5]
+                if self.Xverts[vert][5] == True:
+                    vType = ("X","+")
+                else:
+                    vType = ("X","-")
+
+            if strandNum == 1:
+                strand = (prv1, nxt1)
+            elif strandNum == 2:
+                strand = (prv2, nxt2)
+            else:
+                raise Exception("Error")
+
+            sID1 = self.__strandLabel((prv1,nxt1))
+            sID2 = self.__strandLabel((prv2,nxt2))
+            ret = (*vType, sID1, sID2)
+            yield [*ret,ret[1+strandNum]]
+
+    def formatVertexMerge(self, vertexmergelist):
+        """Takes the output of vertexSequence and prints formatted Mathematica
+        code to stdout."""
+        vType = vertexmergelist[0]
+        vTypeAnnot = vertexmergelist[1]
+        strand1 = vertexmergelist[2]
+        strand2 = vertexmergelist[3]
+        mergeStrand = vertexmergelist[4]
+
+        if vType == 'H':
+            output = " // Subscript[mH," \
+                + str(vTypeAnnot) + "," \
+                + str(strand1) + "," \
+                + str(strand2) + "," \
+                + str(mergeStrand) + "]"
+            sys.stdout.write(output)
+
+        if vType == 'X':
+            if vTypeAnnot == '+':
+                output = " // Subscript[mXp," \
+                    + str(strand1) + "," \
+                    + str(strand2) + "," \
+                    + str(mergeStrand) + "]"
+                sys.stdout.write(output)
+            elif vTypeAnnot == '-':
+                output = " // Subscript[mXn," \
+                    + str(strand1) + "," \
+                    + str(strand2) + "," \
+                    + str(mergeStrand) + "]"
+                sys.stdout.write(output)
+
+
+    def printGamma(self):
+        sys.stdout.write("(* ::Input:: *)\n")
+        sys.stdout.write("(*Subscript[II, s]")
+        gen = self.vertexSequence()
+        for v in gen:
+            self.formatVertexMerge(v)
+        sys.stdout.write("*)\n")
