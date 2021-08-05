9ac558a850d586323dcdc79fd4ebe34bd2b24577
epsilam <2.7182@protonmail.com>
2021-08-05 07:16:16 +0200

    update

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from src.gamma import *
 
-P = ProteinGamma('1R02', np.array([1,-1,1]))
-P.ProProj.plotCrossings()
+PROTEIN_NAME = '1R02'
+PROJECTION_PLANE_NORMAL = np.array([1,-1,1])
+PLOT_GRAPH = False
+
+P = ProteinGamma(PROTEIN_NAME, PROJECTION_PLANE_NORMAL)
+if PLOT_GRAPH:
+    P.ProProj.plotCrossings()
 P.printGamma()
